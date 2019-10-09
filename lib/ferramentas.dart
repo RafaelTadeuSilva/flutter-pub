34eeb548074e9eec8b43242402f00b8964248946
RafaelTadeuSilva <56327128+RafaelTadeuSilva@users.noreply.github.com>
2019-10-09 19:23:58 -0300

    Update ferramentas.dart

--- lib/ferramentas.dart
+++ lib/ferramentas.dart
@@ -1,21 +1,27 @@
-
-
 library ferramentas;
+
 import 'package:ferramentas/src/sign-in.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
 }
 
-
-class SignIn{
-  Future<String> teste(){
+class SignIn {
+  Future<String> teste() {
     return signInWithGoogle();
   }
-  
-  String image() => imageUrl2;
-  String name() => name2;
-  String email() => email2;
 
+  String image() {
+    return imageUrl2;
+  }
+
+  String name() {
+    return name2;
+  }
+
+  String email() {
+    return email2;
+  }
 }
