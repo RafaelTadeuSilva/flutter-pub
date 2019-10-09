dcb48bf8b6f0751c1347443e7f47b3b38fc23b00
RafaelTadeuSilva <56327128+RafaelTadeuSilva@users.noreply.github.com>
2019-10-09 19:01:24 -0300

    teste2

--- lib/ferramentas.dart
+++ lib/ferramentas.dart
@@ -1,7 +1,16 @@
-library ferramentas;
 
+
+library ferramentas;
+import 'package:ferramentas/src/sign-in.dart';
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+
+class SignIn{
+  Future<String> teste(){
+    return signInWithGoogle();
+  }
 }
